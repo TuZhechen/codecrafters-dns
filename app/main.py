7f11ed4f4cc1d6d88f7afa621ef73891e3421afa
Paul Tu <32992631+TuZhechen@user.noreply.github.com>
2023-11-29 09:29:48 +0800

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import socket
-
+from dnslib import DNSRecord, RR, QTYPE
 
 def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,10 +8,11 @@ def main():
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
-    
-            response = b""
-    
-            udp_socket.sendto(response, source)
+
+            query = DNSRecord.parse(buf)
+            response = DNSRecord(DNSHeader(id=query.header.id, qr=1, aa=1, ra=1), q=query.q)
+            response.add_answer(RR(query.q.qname, QTYPE.A, rdata='1.2.3.4'))
+            udp_socket.sendto(response.pack(), source)
         except Exception as e:
             print(f"Error receiving data: {e}")
             break
