6a33afad045063196bcbb307c9f50ff67cd751fe
Paul Tu <32992631+TuZhechen@user.noreply.github.com>
2023-11-29 09:43:37 +0800

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import socket
-from dnslib import DNSRecord, RR, QTYPE
+import struct
 
 def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -9,10 +9,18 @@ def main():
         try:
             buf, source = udp_socket.recvfrom(512)
 
-            query = DNSRecord.parse(buf)
-            response = DNSRecord(DNSHeader(id=query.header.id, qr=1, aa=1, ra=1), q=query.q)
-            response.add_answer(RR(query.q.qname, QTYPE.A, rdata='1.2.3.4'))
-            udp_socket.sendto(response.pack(), source)
+            #Unpack the DNS query header
+            id, flags, qdcount, ancount, nscount, arcount = struct.unpack("!HHHHHH", buf[:12])
+
+            #Create a DNS response header
+            response = struct.pack('!6H', id, 0x8180, qdcount, 1, 0, 0) + buf[12:] + buf[12:-4]
+            response += struct.pack('!2H', 1, 0X0001) # TYPE and CLASS
+            response += struct.pack('!I', 3600) # TTL
+            response += struct.pack('!H', 4) # RDLENGTH
+            response += socket.inet_aton("1.2.3.4") # RDATA
+
+            #Send the DNS response
+            udp_socket.sendto(response, source)
         except Exception as e:
             print(f"Error receiving data: {e}")
             break
